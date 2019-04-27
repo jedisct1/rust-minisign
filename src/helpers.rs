b41ee5eb81542472d30731e0ca9855d111cf0d7a
Frank Denis <github@pureftpd.org>
2019-04-28 00:55:52 +0200

    up

---
+++ src/helpers.rs
@@ -0,0 +1,83 @@
+use crate::perror::*;
+use crate::scrypt::ScryptParams;
+use crate::types::*;
+use std::cmp;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+pub fn store_u64_le(x: u64) -> [u8; 8] {
+    let b1: u8 = (x & 0xff) as u8;
+    let b2: u8 = ((x >> 8) & 0xff) as u8;
+    let b3: u8 = ((x >> 16) & 0xff) as u8;
+    let b4: u8 = ((x >> 24) & 0xff) as u8;
+    let b5: u8 = ((x >> 32) & 0xff) as u8;
+    let b6: u8 = ((x >> 40) & 0xff) as u8;
+    let b7: u8 = ((x >> 48) & 0xff) as u8;
+    let b8: u8 = ((x >> 56) & 0xff) as u8;
+    [b1, b2, b3, b4, b5, b6, b7, b8]
+}
+
+#[allow(clippy::cast_lossless)]
+pub fn load_u64_le(x: &[u8]) -> u64 {
+    (x[0] as u64)
+        | (x[1] as u64) << 8
+        | (x[2] as u64) << 16
+        | (x[3] as u64) << 24
+        | (x[4] as u64) << 32
+        | (x[5] as u64) << 40
+        | (x[6] as u64) << 48
+        | (x[7] as u64) << 56
+}
+
+pub fn raw_scrypt_params(memlimit: usize, opslimit: u64) -> Result<ScryptParams> {
+    let opslimit = cmp::max(32768, opslimit);
+    let mut n_log2 = 1u8;
+    let r = 8u32;
+    let p;
+    if opslimit < (memlimit / 32) as u64 {
+        p = 1;
+        let maxn = opslimit / (u64::from(r) * 4);
+        while n_log2 < 63 {
+            if 1u64 << n_log2 > maxn / 2 {
+                break;
+            }
+            n_log2 += 1;
+        }
+    } else {
+        let maxn = memlimit as u64 / (u64::from(r) * 128);
+        while n_log2 < 63 {
+            if 1u64 << n_log2 > maxn / 2 {
+                break;
+            }
+            n_log2 += 1;
+        }
+        let maxrp = cmp::min(
+            0x3fff_ffff as u32,
+            ((opslimit / 4) / (1u64 << n_log2)) as u32,
+        );
+        p = maxrp / r;
+    }
+    ScryptParams::new(n_log2, r, p).map_err(Into::into)
+}
+
+pub fn get_password(prompt: &str) -> Result<String> {
+    let pwd = rpassword::prompt_password_stdout(prompt)?;
+    if pwd.is_empty() {
+        println!("<empty>");
+        Ok(pwd)
+    } else if pwd.len() > PASSWORDMAXBYTES {
+        Err(PError::new(
+            ErrorKind::Misc,
+            "passphrase can't exceed 1024 bytes length",
+        ))
+    } else {
+        Ok(pwd)
+    }
+}
+
+pub fn unix_timestamp() -> u64 {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("system clock is incorrect");
+    since_the_epoch.as_secs()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,79 +3,24 @@ extern crate rand;
 extern crate rpassword;
 extern crate scrypt;
 
+mod helpers;
+
 pub mod crypto;
 pub mod parse_args;
 pub mod perror;
 pub mod types;
 
 use crate::crypto::ed25519;
+use helpers::*;
 use rand::{thread_rng, RngCore};
-use scrypt::ScryptParams;
-use std::cmp;
 use std::fs::{File, OpenOptions};
 use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use std::path::Path;
-use std::time::{SystemTime, UNIX_EPOCH};
 use std::u64;
 
 pub use crate::parse_args::*;
 pub use crate::perror::*;
 pub use crate::types::*;
-
-fn store_u64_le(x: u64) -> [u8; 8] {
-    let b1: u8 = (x & 0xff) as u8;
-    let b2: u8 = ((x >> 8) & 0xff) as u8;
-    let b3: u8 = ((x >> 16) & 0xff) as u8;
-    let b4: u8 = ((x >> 24) & 0xff) as u8;
-    let b5: u8 = ((x >> 32) & 0xff) as u8;
-    let b6: u8 = ((x >> 40) & 0xff) as u8;
-    let b7: u8 = ((x >> 48) & 0xff) as u8;
-    let b8: u8 = ((x >> 56) & 0xff) as u8;
-    [b1, b2, b3, b4, b5, b6, b7, b8]
-}
-
-#[allow(clippy::cast_lossless)]
-fn load_u64_le(x: &[u8]) -> u64 {
-    (x[0] as u64)
-        | (x[1] as u64) << 8
-        | (x[2] as u64) << 16
-        | (x[3] as u64) << 24
-        | (x[4] as u64) << 32
-        | (x[5] as u64) << 40
-        | (x[6] as u64) << 48
-        | (x[7] as u64) << 56
-}
-
-fn raw_scrypt_params(memlimit: usize, opslimit: u64) -> Result<ScryptParams> {
-    let opslimit = cmp::max(32768, opslimit);
-    let mut n_log2 = 1u8;
-    let r = 8u32;
-    let p;
-    if opslimit < (memlimit / 32) as u64 {
-        p = 1;
-        let maxn = opslimit / (u64::from(r) * 4);
-        while n_log2 < 63 {
-            if 1u64 << n_log2 > maxn / 2 {
-                break;
-            }
-            n_log2 += 1;
-        }
-    } else {
-        let maxn = memlimit as u64 / (u64::from(r) * 128);
-        while n_log2 < 63 {
-            if 1u64 << n_log2 > maxn / 2 {
-                break;
-            }
-            n_log2 += 1;
-        }
-        let maxrp = cmp::min(
-            0x3fff_ffff as u32,
-            ((opslimit / 4) / (1u64 << n_log2)) as u32,
-        );
-        p = maxrp / r;
-    }
-    ScryptParams::new(n_log2, r, p).map_err(Into::into)
-}
 
 pub fn generate_unencrypted_keypair() -> Result<(PublicKey, SecretKey)> {
     let mut seed = vec![0u8; 32];
@@ -109,37 +54,14 @@ pub fn generate_unencrypted_keypair() -> Result<(PublicKey, SecretKey)> {
     Ok((p_struct, s_struct))
 }
 
-fn derive_and_crypt(sk_str: &mut SecretKey, pwd: &[u8]) -> Result<()> {
+fn derive_and_crypt(sk: &mut SecretKey, pwd: &[u8]) -> Result<()> {
     let mut stream = [0u8; CHK_BYTES + SECRETKEYBYTES + KEYNUMBYTES];
-    let opslimit = load_u64_le(&sk_str.kdf_opslimit_le);
-    let memlimit = load_u64_le(&sk_str.kdf_memlimit_le) as usize;
+    let opslimit = load_u64_le(&sk.kdf_opslimit_le);
+    let memlimit = load_u64_le(&sk.kdf_memlimit_le) as usize;
     let params = raw_scrypt_params(memlimit, opslimit)?;
-    scrypt::scrypt(&pwd, &sk_str.kdf_salt, &params, &mut stream)?;
-    sk_str.xor_keynum(&stream);
+    scrypt::scrypt(&pwd, &sk.kdf_salt, &params, &mut stream)?;
+    sk.xor_keynum(&stream);
     Ok(())
-}
-
-fn get_password(prompt: &str) -> Result<String> {
-    let pwd = rpassword::prompt_password_stdout(prompt)?;
-    if pwd.is_empty() {
-        println!("<empty>");
-        Ok(pwd)
-    } else if pwd.len() > PASSWORDMAXBYTES {
-        Err(PError::new(
-            ErrorKind::Misc,
-            "passphrase can't exceed 1024 bytes length",
-        ))
-    } else {
-        Ok(pwd)
-    }
-}
-
-fn unix_timestamp() -> u64 {
-    let start = SystemTime::now();
-    let since_the_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("system clock is incorrect");
-    since_the_epoch.as_secs()
 }
 
 pub fn generate_encrypted_keypair(password: Option<String>) -> Result<(PublicKey, SecretKey)> {
