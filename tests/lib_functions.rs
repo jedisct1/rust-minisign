3bb9838f8cdc525078d87f7ed72ece36b9db4d65
Frank Denis <github@pureftpd.org>
2019-04-27 13:38:56 +0200

    Remove integration tests
    These are better handler with a simple shell script

--- tests/lib_functions.rs
+++ tests/lib_functions.rs
@@ -2,9 +2,9 @@
 extern crate unwrap;
 extern crate base64;
 extern crate libsodium_sys as ffi;
-extern crate rsign;
+extern crate rsign2;
 
-use rsign::*;
+use rsign2::*;
 
 const SK: &[u8] = b"RWRTY0Iyvpg1Je/NLfXYyY9pNMOu6vU1vxGyds3DysvAZ3jvu2oAAAACAAAAAAAAAEAAAAAAuj+eiMbdoT8D5CWnfkVPsrwOOzPs20czHYYJ8cgKX5uvt8hMrwbfbYTEhXJUd4EEUr2yWM4SuaymXZ+8uM2OL7va5HCp33S6HgJqiMdL1O4qQLcwIT2R6/oRsljKAXwSktuTiKV3SVw=";
 const MSG: &[u8] = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a velit at nisi molestie elementum at quis nisi. Quisque vestibulum libero a orci rhoncus elementum. Donec placerat dapibus cursus. Phasellus neque ex, pretium eget tellus tempus, tristique suscipit erat. Nulla volutpat luctus nunc, eu malesuada lacus rutrum eu. Sed a ipsum vel ex cursus condimentum aliquet sit amet nisi. Pellentesque felis sapien, hendrerit ac eleifend eget, rhoncus non tortor. Fusce elementum, velit non ullamcorper luctus, odio sapien consequat augue, vitae condimentum mi enim ac nunc. Ut facilisis eleifend arcu. Ut tincidunt ultrices nibh quis tristique. Nullam sit amet purus accumsan, interdum risus ac, lacinia nulla.\n";
